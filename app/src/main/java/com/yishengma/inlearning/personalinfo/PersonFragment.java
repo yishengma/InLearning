e1f8483ef24cedb98339746a3e75e7cae3769f9e
yishengma <13104862372@163.com>
2020-01-11 11:37:59 +0800

    个人信息界面

---
+++ app/src/main/java/com/yishengma/inlearning/personalinfo/PersonFragment.java
@@ -0,0 +1,20 @@
+package com.yishengma.inlearning.personalinfo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yishengma.inlearning.home.BaseFragment;
+
+public class PersonFragment extends BaseFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+
+}
