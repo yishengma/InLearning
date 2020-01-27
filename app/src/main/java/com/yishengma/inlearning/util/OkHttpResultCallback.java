9a499aeb10888d69f4729516bcbf25cd9d427045
yishengma <13104862372@163.com>
2020-01-27 16:19:20 +0800

    添加Okhttp网络请求库

---
+++ app/src/main/java/com/yishengma/inlearning/util/OkHttpResultCallback.java
@@ -0,0 +1,11 @@
+package com.yishengma.inlearning.util;
+
+
+import okhttp3.Call;
+
+public interface  OkHttpResultCallback {
+
+    void onError(Call call, Exception e);
+
+    void onResponse(byte[] bytes);
+}
